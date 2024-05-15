e8417fde3773b3a35ac82a0ab965d3128dbc3a05
BinHenz <dstjer@naver.com>
2024-05-16 00:07:41 +0900

    RifleFire Ability Collision Apply & ZombieAIController Add
    라이플 사격 어빌리티 히트판정 콜리전 설정
    좀비 AI 컨트롤러 C++ 생성

---
+++ Source/ProjectZZ/Public/AI/ZZZombieAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ZZZombieAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZZ_API AZZZombieAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ProjectZZ/Public/Character/ZZZombieCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ProjectZZCharacter.h"
+#include "ZZZombieCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTZZ_API AZZZombieCharacter : public AProjectZZCharacter
+{
+	GENERATED_BODY()
+	
+};
